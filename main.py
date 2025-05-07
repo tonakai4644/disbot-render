0ac49ac31adbd2b22f5984a8743197d89a436c7e
tonakai4644 <tonakai4644@gmail.com>
2025-05-07 16:01:10 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import os
+from keep import keep_alive
+
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print(f'ログインしました: {self.user}')
+
+    async def on_message(self, message):
+        print(f'送信: {message.author}: {message.content}')
+        if message.author == self.user:
+            return
+
+        if message.content == '$Hello':
+            await message.channel.send('Hello!')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+keep_alive()
+try:
+    client.run(os.environ['TOKEN'])
+except Exception as e:
+    print(f"Bot実行中にエラーが発生しました: {e}")
